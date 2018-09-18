826b4fa837bda64ed4cef6040f1473650ab55a8a
Lauren <laurengilbert1997@gmail.com>
2018-09-18 14:44:00 +1200

    Added Font awesome

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './../node_modules/bulma/css/bulma.css';
+import './../node_modules/bulma/css/bulma.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+//add font awesome icons here example:
+library.add(faCoffee)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
