f6595b6e90ffea85bdd90ff2c46f3e66df3e916a
banjo <bvckbarnes@gmail.com>
2018-09-21 09:39:15 +1200

    bs

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,55 +1,61 @@
 <template>
   <div class="hello">
     <div class="hero-image">
-
-    <h2>b&emacr;on</h2>
-    <p>{{ designers.clairehartley.data }}</p>
+   
+      <h2>b&emacr;on</h2>
+      <p>{{ designers.clairehartley.data }}</p>
   </div>
   </div>
 </template>
 
 
 <script>
-import axios from 'axios'
-import Vue from 'vue'
-window.Vue = Vue
+import axios from "axios";
+import Vue from "vue";
+window.Vue = Vue;
 export default {
-  name: 'home',
-  data () {
+  name: "home",
+  data() {
     return {
       designers: []
-    }
+    };
   },
 
   methods: {
-    getDisignerProjects: function (userID) {
-      axios.get('https://cors-anywhere.herokuapp.com/http://www.behance.net/v2/users/' + userID + '?api_key=UhOrt3HySq95LUrfQWErTpR5KK12oq2Q')
+    getDisignerProjects: function(userID) {
+      axios
+        .get(
+          "https://cors-anywhere.herokuapp.com/http://www.behance.net/v2/users/" +
+            userID +
+            "?api_key=UhOrt3HySq95LUrfQWErTpR5KK12oq2Q"
+        )
         .then(response => {
-          this.designers[userID] = response
-          this.$forceUpdate()
-          console.log(this.designers)
-        }).catch(error => console.log(error.stack))
-        
+          this.designers[userID] = response;
+          this.$forceUpdate();
+          console.log(this.designers);
+        })
+        .catch(error => console.log(error.stack));
     }
   },
 
-  mounted () {
-    this.getDisignerProjects('Lukas Furlan')
-    this.getDisignerProjects('Bao Luu')
-    this.getDisignerProjects('Emily Soto')
-    this.getDisignerProjects('Reuben Wu')
+  mounted() {
+    this.getDisignerProjects("Lukas Furlan");
+    this.getDisignerProjects("Bao Luu");
+    this.getDisignerProjects("Emily Soto");
+    this.getDisignerProjects("Reuben Wu");
 
     /*  axios.get('https://cors-anywhere.herokuapp.com/http://www.behance.net/v2/users/eldiablo?api_key=UhOrt3HySq95LUrfQWErTpR5KK12oq2Q')
     .then(response => {
       console.log(response)
     }).catch(error => console.log(error.stack)) */
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
@@ -60,6 +66,4 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-
-
 </style>
