7904a94698326172f25d73a75f50e476869c39f9
banjo <bvckbarnes@gmail.com>
2018-09-27 12:52:04 +1200

    merga

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,139 +1,128 @@
 <template>
-<div class="hello">
-    <div class="hero-image">
-      <div class="hero">
-        <img src="../assets/Hero.png"/>
-          <div class="text">
-            <h1>bēon</h1>
-            <h3>Dēsign Agēncy</h3>
-            <h4>Profssional Portfolios</h4>
-            <div class="whiteIcons">
-          <div class="number"><img id="email" src="../assets/white-email.png"/>
-           <p>bēon@design.co.nz</p>
-           </div>
-          <div class="number"><img id="phone" src="../assets/white-phone.png"/>
-          <p>03 321 12345</p>
-          </div>
-            </div>  
+    <div class="hello">
+        <div class="hero-image">
+            <div class="hero">
+                <img src="../assets/Hero.png" />
+                <div class="text">
+                    <h1>bēon</h1>
+                    <h3>Dēsign Agēncy</h3>
+                    <h4>Profssional Portfolios</h4>
+                    <div class="whiteIcons">
+                        <div class="number"><img id="email" src="../assets/white-email.png" />
+                            <p>bēon@design.co.nz</p>
+                        </div>
+                        <div class="number"><img id="phone" src="../assets/white-phone.png" />
+                            <p>03 321 12345</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="Scroll-arrow">
+                    <img class="arrow" src="../assets/arrow.png" />
+                </div>
 
+                <div class="container">
+                    <div class="columns">
+                        <div class="column">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-left">
+                                        </div>
+                                        <div class="media-content">
+                                            <p class="title is-4">John Smith</p>
+                                        </div>
+                                    </div>
 
-            <div class="text">
-                <h1>bēon</h1>
-                <h3>Design agency</h3>
-                <h4>Meet our designers</h4>
-                <div class="hero-icons">
-                    <img class="email" src="../assets/white-email.png" />
-                    <img class="phone" src="../assets/white-phone.png" />
+                                    <div class="content">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-left">
+                                        </div>
+
+                                        <div class="media-content">
+                                            <p class="title is-4">John Smith</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="content">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-left">
+                                        </div>
+                                        <div class="media-content">
+                                            <p class="title is-4">John Smith</p>
+                                        </div>
+                                    </div>
+
+                                    <div class="content">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-4by3">
+                                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <div class="media">
+                                        <div class="media-left"> </div>
+                                        <div class="media-content">
+                                            <p class="title is-4">John Smith</p>
+                                        </div>
+                                    </div>
+                                    <div class="content">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
 
         </div>
-        <div class="Scroll-arrow">
-            <img class="arrow" src="../assets/arrow.png" />
 
-        </div>
-        <div class="container">
-            <div class="columns">
-                <div class="column">
-                    <div class="card">
-                        <div class="card-image">
-                            <figure class="image is-4by3">
-                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                            </figure>
-                        </div>
-                        <div class="card-content">
-                            <div class="media">
-                                <div class="media-left">
-                                </div>
-                                <div class="media-content">
-                                    <p class="title is-4">John Smith</p>
-                                </div>
-                            </div>
-
-                            <div class="content">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="card">
-                        <div class="card-image">
-                            <figure class="image is-4by3">
-                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                            </figure>
-                        </div>
-                        <div class="card-content">
-                            <div class="media">
-                                <div class="media-left">
-                                </div>
-
-                                <div class="media-content">
-                                    <p class="title is-4">John Smith</p>
-                                </div>
-                            </div>
-
-                            <div class="content">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="card">
-                        <div class="card-image">
-                            <figure class="image is-4by3">
-                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                            </figure>
-                        </div>
-                        <div class="card-content">
-                            <div class="media">
-                                <div class="media-left">
-                                </div>
-                                <div class="media-content">
-                                    <p class="title is-4">John Smith</p>
-                                </div>
-                            </div>
-
-                            <div class="content">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="card">
-                        <div class="card-image">
-                            <figure class="image is-4by3">
-                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                            </figure>
-                        </div>
-                        <div class="card-content">
-                            <div class="media">
-                                <div class="media-left"> </div>
-                                <div class="media-content">
-                                    <p class="title is-4">John Smith</p>
-                                </div>
-                            </div>
-                            <div class="content">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
+        <h2></h2>
+        <p>{{ designers.zhelieznova.data }}</p>
     </div>
-    <h2></h2>
-    <p>{{ designers.zhelieznova.data }}</p>
-</div>
 
-   
 </template>
 
 
@@ -246,34 +235,6 @@ h4 {
 p {
   color: white;
   margin: 12px;
-=======
-.phone {
-  height: 50px;
-  width: 50px;
-  margin-top: -50px;
-}
-.email {
-  height: 50px;
-  width: 50px;
-  margin-top: -50px;
-}
-.hero-icon {
-  margin-bottom: 50px;
-
-}
-.Scroll-arrow {
-  padding-bottom: 50px;
-  padding: 50px 20px 50px;
-  text-align: center;
-}
-.arrow {
-  height: 50px;
-  width: 50px;
-  
-}
-.container {
-  background-color: #F7F7F7;
-  padding: 30px;
 }
 
 </style>
